4137286aab199ce9d567cc3b85078dc7913d69e5
Bugbuster2004 <siddhant6338bansal@gmail.com>
2024-09-08 19:14:01 +0530

    added pages

---
+++ admin/doccure.dreamstechnologies.com/html/template/admin/js/src/docData.js
@@ -0,0 +1,67 @@
+// doctors data
+const doctors = [
+  {
+    name: "Dr. A",
+    speciality: "Dental",
+    earned: "Rs. 3200.00",
+    reviews: 4,
+  },
+  {
+    name: "Dr. B",
+    speciality: "Dental",
+    earned: "Rs. 3100.00",
+    reviews: 4,
+  },
+  {
+    name: "Dr. C",
+    speciality: "Cardiology",
+    earned: "Rs. 4000.00",
+    reviews: 4,
+  },
+  {
+    name: "Dr. D",
+    speciality: "Urology",
+    earned: "Rs. 3200.00",
+    reviews: 4,
+  },
+  {
+    name: "Dr. E",
+    speciality: "Orthopaedics",
+    earned: "Rs. 3500.00",
+    reviews: 4,
+  },
+];
+
+// patients data
+const patients = [
+  {
+    name: "Patient A",
+    phone: "8286329170",
+    lastVisit: "20 Oct 2023",
+    paid: "Rs. 100.00",
+  },
+  {
+    name: "Patient B",
+    phone: "2077299974",
+    lastVisit: "22 Oct 2023",
+    paid: "Rs. 200.00",
+  },
+  {
+    name: "Patient C",
+    phone: "2607247769",
+    lastVisit: "21 Oct 2023",
+    paid: "Rs. 250.00",
+  },
+  {
+    name: "Patient D",
+    phone: "5043686874",
+    lastVisit: "21 Sep 2023",
+    paid: "Rs. 150.00",
+  },
+  {
+    name: "Patient E",
+    phone: "9548207887",
+    lastVisit: "18 Sep 2023",
+    paid: "Rs. 350.00",
+  },
+];
